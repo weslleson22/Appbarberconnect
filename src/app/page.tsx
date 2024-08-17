18151bd45f4c881ae46da52e80a064a6fde31845
weslleson22 <wesllesonsowza@gmail.com>
2024-08-16 23:52:07 -0300

    feat: add componentes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import Header from "./_components/header"
 
-import { Button } from "./_components/ui/button"
-
-const Home = ()=>{
-
-  return <Button>Teste</Button>
-  }
+const Home = () => {
+  return (
+    <div>
+      <Header></Header>
+    </div>
+  )
+}
 export default Home

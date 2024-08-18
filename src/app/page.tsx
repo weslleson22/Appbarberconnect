983d1b5e0342180280aa810cdb5df8d70c0617ae
weslleson22 <wesllesonsowza@gmail.com>
2024-08-17 22:22:50 -0300

    feat: add header component and banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,32 @@
+import { SearchIcon } from "lucide-react"
 import Header from "./_components/header"
+import { Button } from "./_components/ui/button"
+import { Input } from "./_components/ui/input"
+import Image from "next/image"
 
 const Home = () => {
   return (
     <div>
       <Header></Header>
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Wesleson!</h2>
+        <p>Sexta-feira, 17 de agosto.</p>
+        <div className="gap-1=2 mt-6 flex items-center gap-8">
+          <Input placeholder="Faça a sua buscar" />
+          <Button>
+            <SearchIcon></SearchIcon>
+          </Button>
+        </div>
+
+        <div className="relative mt-6 h-[150px] w-full">
+          <Image
+            alt="Agende nos melhores com Barber"
+            src="/banner-01.png"
+            fill
+            className="rounded-xl object-cover"
+          />
+        </div>
+      </div>
     </div>
   )
 }

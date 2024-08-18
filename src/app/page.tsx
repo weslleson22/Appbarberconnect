222f204036f452cef7e5ede4bd967d380fc9c4a0
weslleson22 <wesllesonsowza@gmail.com>
2024-08-17 23:43:46 -0300

    feat: add bookerr

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,10 @@ import Header from "./_components/header"
 import { Button } from "./_components/ui/button"
 import { Input } from "./_components/ui/input"
 import Image from "next/image"
+import { Card, CardContent } from "./_components/ui/card"
+import { Badge } from "./_components/ui/badge"
+import { Avatar } from "@radix-ui/react-avatar"
+import { AvatarImage } from "./_components/ui/avatar"
 
 const Home = () => {
   return (
@@ -26,6 +30,27 @@ const Home = () => {
             className="rounded-xl object-cover"
           />
         </div>
+
+        <Card className="mt-6">
+          <CardContent className="flex justify-between p-0">
+            <div className="flex flex-col gap-2 py-5 pl-5">
+              <Badge className="w-fit">Confirmado</Badge>
+              <h3 className="font-semibold">Corte de Cabelo</h3>
+
+              <div className="flex items-center gap-2">
+                <Avatar className="h-6 w-6">
+                  <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png"></AvatarImage>
+                </Avatar>
+                <p className="text-sm">Barbearia Wesleson</p>
+              </div>
+            </div>
+            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
+              <p className="text-sm">Agosto</p>
+              <p className="text-2xl">05</p>
+              <p className="text-sm">20:00</p>
+            </div>
+          </CardContent>
+        </Card>
       </div>
     </div>
   )
